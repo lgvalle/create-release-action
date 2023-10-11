66e75f1e9a538cb99091bd417e3ff95dda066020
Luis G. Valle <354372+lgvalle@users.noreply.github.com>
2023-10-11 16:37:52 +0200

    Update script

--- src/commitlog.sh
+++ src/commitlog.sh
@@ -29,7 +29,6 @@ else
   git_command="git log $previous_tag..$current_branch $commit_log_format"
 fi
 
-echo "Executing git command: $git_command"
 commit_log=$(eval $git_command)
 
 echo "$commit_log"
