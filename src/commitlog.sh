7a94ccba5dc9cbb6d1cf3c86c29fc68ae24fcb0b
Luis G. Valle <354372+lgvalle@users.noreply.github.com>
2023-10-11 17:27:54 +0200

    debug

--- src/commitlog.sh
+++ src/commitlog.sh
@@ -29,9 +29,4 @@ else
   git_command="git log $previous_tag..$current_branch $commit_log_format"
 fi
 
-echo "Git command $git_command"
-
-
-commit_log=$(eval $git_command)
-
-echo "$commit_log"
+echo "$git_command"
