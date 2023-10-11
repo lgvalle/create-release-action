e0f590b0f64972a1fd2ce0c29a98ffed78061dbf
Luis G. Valle <354372+lgvalle@users.noreply.github.com>
2023-10-11 17:21:05 +0200

    Add fetch

--- src/commitlog.sh
+++ src/commitlog.sh
@@ -29,6 +29,9 @@ else
   git_command="git log $previous_tag..$current_branch $commit_log_format"
 fi
 
+echo "Git command"
+echo "$git_command"
+
 commit_log=$(eval $git_command)
 
 echo "$commit_log"
