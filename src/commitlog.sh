2f3c4b4473d3e197ac410e667c740fd1295a2c37
Luis G. Valle <354372+lgvalle@users.noreply.github.com>
2023-10-11 16:17:54 +0200

    Update script

--- src/commitlog.sh
+++ src/commitlog.sh
@@ -2,22 +2,13 @@
 
 current_branch=$1
 
+# Define the git format
+commit_log_format="--pretty=format:\"%h - %an - %ad - %s\" --date=default"
+
 if [ -z "$current_branch" ]; then
   echo "Error: Missing current branch name." >&2
   exit 1
 fi
-
-# Determine the default branch
-default_branch=$(git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | awk -F/ '{print $NF}')
-if [ -z "$default_branch" ]; then
-  echo "Error: Failed to determine the default branch." >&2
-  exit 2
-fi
-
-# Function to get the tagger date of a tag
-get_tagger_date() {
-  git show -s --format="%ci" $1 2>/dev/null
-}
 
 # Get a list of all tags sorted by tagger date
 tags_sorted_by_date=$(git tag --sort='-creatordate')
@@ -25,20 +16,19 @@ tags_sorted_by_date=$(git tag --sort='-creatordate')
 # Find the previous tag based on the tagger date
 previous_tag=""
 for tag in $tags_sorted_by_date; do
-  branch_contains_tag=$(git branch --contains $tag | grep -q "$current_branch" && echo "yes" || echo "no")
-  if [ "$branch_contains_tag" == "yes" ]; then
+  if git branch --contains $tag | grep -q "$current_branch"; then
     previous_tag=$tag
     break
   fi
 done
 
+# Calculate the commit log
 if [ -z "$previous_tag" ]; then
-  echo "No previous tag found for branch $current_branch. Comparing with the default branch ($default_branch)."
-  previous_tag=$default_branch
+  git_command="git log $commit_log_format"
+else
+  git_command="git log $previous_tag..$current_branch $commit_log_format"
 fi
 
-# Calculate the commit log
-git_command="git log $previous_tag..$current_branch --pretty=format:\"%h - %an - %ad - %s\" --date=default"
 echo "Executing git command: $git_command"
 commit_log=$(eval $git_command)
 
