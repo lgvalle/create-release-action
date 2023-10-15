52cbd5e2d2b314bf2c81d3bf2931a7e3f94617b9
Luis G. Valle <354372+lgvalle@users.noreply.github.com>
2023-10-15 09:53:34 +0200

    Add line break to formatter

--- src/commitlog.sh
+++ src/commitlog.sh
@@ -3,7 +3,7 @@
 current_branch=$1
 
 # Define the git format
-commit_log_format="--pretty=format:\"%h - %an - %ad - %s\" --date=default"
+commit_log_format="--pretty=format:\"%h - %an - %ad - %s%n\" --date=default"
 
 if [ -z "$current_branch" ]; then
   echo "Error: Missing current branch name." >&2
