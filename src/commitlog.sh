a57f03680a87c118b8e9646b9058163e41d8f182
Luis G. Valle <354372+lgvalle@users.noreply.github.com>
2023-10-15 09:29:52 +0200

    Debug script

--- src/commitlog.sh
+++ src/commitlog.sh
@@ -35,7 +35,10 @@ else
   git_command="git log $previous_tag..$current_branch $commit_log_format"
 fi
 
+echo "XXX git command == $git_command"
 
 commit_log=$(eval $git_command)
+
+echo "XXX commit log == $commit_log"
 
 echo $commit_log
